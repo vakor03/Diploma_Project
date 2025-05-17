9eb240fe4a0d4544d6446dad35da5ddcfef5203d
valerii.korniienko <vakor.dev@gmail.com>
2025-05-17 12:55:45 +0300

    :hammer: Code cleanup

--- src/Diploma_v2/Assets/_Project/Features/EnemyModule/Scripts/EnemyPool/EnemyPoolInstaller.cs
+++ src/Diploma_v2/Assets/_Project/Features/EnemyModule/Scripts/EnemyPool/EnemyPoolInstaller.cs
@@ -1,24 +1,18 @@
 ﻿using _Project.Extensions.ZenjectExtensions;
 using _Project.Features.ObjectPoolModule;
-using _Project.Features.StatsModule;
 using _Project.Scripts.Infrastructure.AssetProviders;
-using UnityEngine;
 using Zenject;
 
 namespace _Project.Features.Enemy {
     public class EnemyPoolInstaller : Installer<EnemyPoolInstaller> {
-
         public override void InstallBindings() {
             Container.BindInterfacesAndSelfToFromAddressables<EnemyPoolConfiguration>(AssetPath.Configuration.ENEMY_POOL_CONFIGURATION)
                 .AsSingle();
+
             Container
                 .Bind<IGenericPooledObjectFactory<MonoPooledEnemy, EnemyType>>()
                 .To<EnemyPooledObjectFactory>()
                 .AsSingle();
-
-
-            // Container.BindInterfacesAndSelfTo<GenericPooledObjectFactory<MonoPooledEnemy, EnemyType>>()
-            //     .AsSingle();
 
             Container.BindInterfacesAndSelfTo<GenericPoolFactory<MonoPooledEnemy, EnemyType>>()
                 .AsSingle();
@@ -27,41 +21,4 @@ namespace _Project.Features.Enemy {
                 .AsSingle();
         }
     }
-
-
-    public class EnemyPooledObjectFactory 
-        : IGenericPooledObjectFactory<MonoPooledEnemy, EnemyType>
-    {
-        readonly DiContainer                          _parentContainer;
-        readonly IObjectPoolConfiguration<EnemyType> _poolConfig;
-
-        [Inject]
-        public EnemyPooledObjectFactory(
-            DiContainer parentContainer,
-            IObjectPoolConfiguration<EnemyType> poolConfig)
-        {
-            _parentContainer = parentContainer;
-            _poolConfig      = poolConfig;
-        }
-
-        public MonoPooledEnemy Create(EnemyType type, Transform parentTransform)
-        {
-            ObjectPoolSettings cfg         = _poolConfig.GetPoolConfiguration(type);
-            GameObject prefab      = cfg.Prefab;
-            StatDataHolder<EntityStats> statsHolder = new StatDataHolder<EntityStats>();
-            statsHolder.SetStat(EntityStats.MaxHealth, 20f);
-        
-            DiContainer subContainer = _parentContainer.CreateSubContainer();
-
-            subContainer.BindInstance(statsHolder).AsSingle();
-
-            GameObject go = subContainer.InstantiatePrefab(prefab, parentTransform);
-
-            MonoPooledEnemy monoPooledEnemy = go.GetComponent<MonoPooledEnemy>();
-            monoPooledEnemy.Type = type;
-            return monoPooledEnemy;
-        }
-    }
-
-
 }
---
+++ src/Diploma_v2/Assets/_Project/Features/EnemyModule/Scripts/EnemyPool/EnemyPooledObjectFactory.cs
@@ -0,0 +1,35 @@
+﻿using _Project.Features.ObjectPoolModule;
+using _Project.Features.StatsModule;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Features.Enemy {
+    public class EnemyPooledObjectFactory : IGenericPooledObjectFactory<MonoPooledEnemy, EnemyType> {
+        readonly DiContainer _parentContainer;
+        readonly IObjectPoolConfiguration<EnemyType> _poolConfig;
+
+        public EnemyPooledObjectFactory(
+            DiContainer parentContainer,
+            IObjectPoolConfiguration<EnemyType> poolConfig) {
+            _parentContainer = parentContainer;
+            _poolConfig = poolConfig;
+        }
+
+        public MonoPooledEnemy Create(EnemyType type, Transform parentTransform) {
+            ObjectPoolSettings cfg = _poolConfig.GetPoolConfiguration(type);
+            GameObject prefab = cfg.Prefab;
+            StatDataHolder<EntityStats> statsHolder = new StatDataHolder<EntityStats>();
+            statsHolder.SetStat(EntityStats.MaxHealth, 20f);
+
+            DiContainer subContainer = _parentContainer.CreateSubContainer();
+
+            subContainer.BindInstance(statsHolder).AsSingle();
+
+            GameObject go = subContainer.InstantiatePrefab(prefab, parentTransform);
+
+            MonoPooledEnemy monoPooledEnemy = go.GetComponent<MonoPooledEnemy>();
+            monoPooledEnemy.Type = type;
+            return monoPooledEnemy;
+        }
+    }
+}
